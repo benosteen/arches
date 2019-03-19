4a11bf8b2ea4be07576db6408530f3cb92fecfc2
benosteen <bosteen@gmail.com>
2019-03-19 08:40:17 -0700

    Adding some basic cypress tests

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,16 @@
-describe('Hello World', function() {
-  it('Tests to see if basic Cypress functionality works', function() {
-    expect(true).to.equal(true)
+describe('Home page', function() {
+  it('Visits the home page', function() {
+    cy.visit('/')
+  })
+})
+
+describe('Log in as admin', function() {
+  it('Goes to Login page from home page', function() {
+    cy.visit('/auth/?next=/index.htm')
+
+    cy.get(':nth-child(3) > .input-group > .form-control').type("admin")
+    cy.get(':nth-child(4) > .input-group > .form-control').type(`admin{enter}`)
+
+    cy.url().should("include", "/index.htm")  // redirected back to home page
   })
 })
